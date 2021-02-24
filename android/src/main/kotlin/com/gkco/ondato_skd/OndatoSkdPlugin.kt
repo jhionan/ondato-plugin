d499eb7e891d37795635ea48f95b3f1d879149d6
Jhionan <jhionan@gmail.com>
2021-02-24 16:29:48 -0300

    feat: add some configuration for ondato android native sdk

--- android/src/main/kotlin/com/gkco/ondato_skd/OndatoSkdPlugin.kt
+++ android/src/main/kotlin/com/gkco/ondato_skd/OndatoSkdPlugin.kt
@@ -16,21 +16,38 @@ class OndatoSkdPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var mContext : Context
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ondato_skd")
     channel.setMethodCallHandler(this)
+    mContext = flutterPluginBinding.applicationContext
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
+      if (call.method == "startIdentification") {
+        startIdentification()
+      }
       result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  private fun startIdentification(){
+    Ondato.starIdentification(mContext, object : Ondato.ResultListener {
+      override fun onSuccess(identificationId: String?) {
+        print("onSuccess")
+      }
+
+      override fun onFailure(identificationId: String?, error: OndatoError) {
+          print("onFailure")
+      }
+  })
   }
 }
