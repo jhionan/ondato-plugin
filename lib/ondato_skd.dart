e2046cedb2be4c6739f41fa136b55d4bbfa5a8d9
Jhionan <jhionan@gmail.com>
2021-02-24 15:25:48 -0300

    feat(channel): create dart implementation of channel

--- lib/ondato_skd.dart
+++ lib/ondato_skd.dart
@@ -1,14 +1,45 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:ondato_skd/ondato_config_model.dart';
 
 class OndatoSkd {
-  static const MethodChannel _channel =
-      const MethodChannel('ondato_skd');
+  static const MethodChannel _channel = const MethodChannel('ondato_skd');
+  static bool _isInit = false;
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version =
+        await _channel.invokeMethod(_OndatoSdkChannel.getPlatformVersion);
     return version;
   }
+
+  static Future<bool> init(OndatoServiceConfiguration config) async {
+    _isInit = await _channel.invokeMethod<bool>(_OndatoSdkChannel.init);
+    return _isInit;
+  }
+
+  static Stream<String> startIdentification() async* {
+    yield* _channel
+        .invokeMethod(_OndatoSdkChannel.startIdentification)
+        .asStream();
+  }
+/* 
+ Ondato.init(config)
+        Ondato.starIdentification(this, object : Ondato.ResultListener {
+            override fun onSuccess(identificationId: String?) {
+                //Success
+            }
+
+            override fun onFailure(identificationId: String?, error: OndatoError) {
+                //Failure
+            }
+        })
+ */
+}
+
+class _OndatoSdkChannel {
+  _OndatoSdkChannel._();
+  static const String getPlatformVersion = 'getPlatformVersion';
+  static const String init = 'init';
+  static const String startIdentification = 'startIdentification';
 }
