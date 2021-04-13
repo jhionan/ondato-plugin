0d2bbbaeaeb6d34aa609577a0e43af37c68d397c
Jhionan <jhionan@gmail.com>
2021-04-13 10:10:33 -0300

    update android sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -61,10 +61,10 @@ class _MyAppState extends State<MyApp> {
                   await OndatoSkd.init(
                     OndatoServiceConfiguration(
                       credentials: OndataCredencials(
-                          accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IkE0QkU3OUQ0NTZCNDQ3NTk2Rjk3MzY5Q0E4QzhBMEU2NkRGRUQ1M0IiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJwTDU1MUZhMFIxbHZsemFjcU1pZzVtMy0xVHMifQ.eyJuYmYiOjE2MTc4OTY3NjksImV4cCI6MTYxNzk4MzE2OSwiaXNzIjoiaHR0cHM6Ly9pZC5vbmRhdG8uY29tIiwiYXVkIjpbImh0dHBzOi8vaWQub25kYXRvLmNvbS9yZXNvdXJjZXMiLCJ2ZXJpZmlkX3B1YmxpY19hcGkiXSwiY2xpZW50X2lkIjoiYXBwLm9uZGF0by5jcmVkaXRpbmZvamFtYWljYWRlbW8iLCJjbGllbnRfYXBwbGljYXRpb25faWQiOiI1Y2JiM2QxYi00YmQwLTQ2NzgtOTAzYS1lYWM3ZmQ4YjJjYjQiLCJzY29wZSI6WyJ2ZXJpZmlkX3B1YmxpY19hcGkiXX0.ua1IaQabKvFa-GtQL872avm8wMm22NuH5VNOpKkpuiAl8WKEDHQVnHIVnfawQBtA1BdzYRgvOxghHvKeOvV0pGbapgXHQ4c3qkQigdbqouo3Y7gMlq6H_3U5gvUC5ql4StNRsqM9V0ta47X7Oai088vb56a4-ioac_pHhOOnMqNN3T-Vga0dpoNfiF20AgCl3A2tbZfNvxuWzaFzQ9UPb4kFmav-bMlvCRkWFTRC7fGxfM9CE6Gp72l6oS_xPQRqAj0I-cbFYDkG59ngbDy1ANDikiT8zvCZevSdf-Xddhffz3Ohw_echz82LPBrxcFqf9PmwJxuZ7waoAciTIpfWQ',
-                          identificationId: '1477e4d54170442f97dbeff3808283d8'),
+                          accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IkE0QkU3OUQ0NTZCNDQ3NTk2Rjk3MzY5Q0E4QzhBMEU2NkRGRUQ1M0IiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJwTDU1MUZhMFIxbHZsemFjcU1pZzVtMy0xVHMifQ.eyJuYmYiOjE2MTgyMzU0NjAsImV4cCI6MTYxODMyMTg2MCwiaXNzIjoiaHR0cHM6Ly9pZC5vbmRhdG8uY29tIiwiYXVkIjpbImh0dHBzOi8vaWQub25kYXRvLmNvbS9yZXNvdXJjZXMiLCJ2ZXJpZmlkX3B1YmxpY19hcGkiXSwiY2xpZW50X2lkIjoiYXBwLm9uZGF0by5jcmVkaXRpbmZvamFtYWljYWRlbW8iLCJjbGllbnRfYXBwbGljYXRpb25faWQiOiI1Y2JiM2QxYi00YmQwLTQ2NzgtOTAzYS1lYWM3ZmQ4YjJjYjQiLCJzY29wZSI6WyJ2ZXJpZmlkX3B1YmxpY19hcGkiXX0.nU_sxMdJxeA7uXBidjk9Mregip4IgH1dxqzQPwLwCyp4EJaeQsE1gSme4Hn0dF5TETZcI9KRNkR1wwJlRHgDvqjZjaUid1YY8iZJ8Nmc7gtiEt-F4U7WWVxb7nCwzhWW-HBZ3oyNWoW7aQm2558z_b2ll9vKSa4mGSFSxozEwCwwlQyJAGhXci1YK3UMckOzYTGi0sS8H2RjY6R8jWp55LsZJ0VPjJVQgYErBpMDG_cHH5G3ehs_RWJcX2o5LqgsSw2UDDLIY8vbAAU5E4YR6aCM2YfH_7kWAHTigVFgIyY-Kq_8_WpTpsraQ_w4e1dMfVemOcM4uSrf9RfZMPsAVA',
+                          identificationId: '0fe67a1171424a08ad8244f480707ea2'),
                       mode: OndatoEnvironment.live,
-                      appearance: OndatoIosAppearance(
+                      appearance: OndatoIosAppearance(  
                           buttonColor: Colors.redAccent,
                           logoImageBase64: base64.encode((await rootBundle
                                   .load('assets/grace_kennedy_logo.jpg'))
